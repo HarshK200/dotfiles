b3e423b651bd8446df8a7dd0b05fc5705c020fc8
HarshK200 <harshkapadia04@gmail.com>
2024-02-21 19:18:00 +0530

    removed nvim(copy)

--- .config/nvim (copy)/lua/kouShinji/plugins/fidget.lua
+++
@@ -1,7 +0,0 @@
-return {
-    -- The little lsp server loading progress thigi that appears on the bottom right of the screen
-    "j-hui/fidget.nvim",
-    config = function()
-        require("fidget").setup({})
-    end,
-}
--- .config/nvim (copy)/lua/kouShinji/plugins/undotree.lua
+++
@@ -1,8 +0,0 @@
-
-return {
-    "mbbill/undotree",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end
-}
