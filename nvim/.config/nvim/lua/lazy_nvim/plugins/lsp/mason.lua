42310520cf7a1185762616b589949c8102b00e39
HarshK200 <harshkapadia04@gmail.com>
2024-09-09 07:46:55 +0530

    removed mason warning

--- nvim/.config/nvim/lua/lazy_nvim/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/lazy_nvim/plugins/lsp/mason.lua
@@ -23,7 +23,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
         "html",
         "tailwindcss",
         "lua_ls",
