0afce26cafd6ae8413fbc8be5c2b10fc90b82f0f
HarshK200 <harshkapadia04@gmail.com>
2024-09-15 17:38:44 +0530

    todo comments config

--- nvim/.config/nvim/lua/lazy_nvim/plugins/todocomments.lua
+++ nvim/.config/nvim/lua/lazy_nvim/plugins/todocomments.lua
@@ -1,21 +1,76 @@
 return {
-  "folke/todo-comments.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local todo_comments = require("todo-comments")
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo_comments = require("todo-comments")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
+		keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
 
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
+		keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
 
-    todo_comments.setup()
-  end,
+		todo_comments.setup({
+			signs = true, -- show icons in the signs column
+			sign_priority = 8, -- sign priority
+			-- keywords recognized as todo comments
+			keywords = {
+				FIX = {
+					icon = " ", -- icon used for the sign, and in search results
+					color = "error", -- can be a hex color, or a named color (see below)
+					alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR", "ERR" }, -- a set of other keywords that all map to this FIX keywords
+				},
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+				TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+			gui_style = {
+				fg = "NONE",
+				bg = "BOLD",
+			},
+			merge_keywords = true,
+			highlight = {
+				multiline = true, -- enable multine todo comments
+				multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+				multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+				before = "", -- "fg" or "bg" or empty
+				keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+				after = "fg", -- "fg" or "bg" or empty
+				pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+				comments_only = true, -- uses treesitter to match keywords in comments only
+				max_line_len = 400, -- ignore lines longer than this
+				exclude = {}, -- list of file types to exclude highlighting
+			},
+			-- list of named colors where we try to extract the guifg from the
+			-- list of highlight groups or use the hex color if hl not found as a fallback
+			colors = {
+				error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+				warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+				info = { "DiagnosticInfo", "#2563EB" },
+				hint = { "DiagnosticHint", "#10B981" },
+				default = { "Identifier", "#7C3AED" },
+				test = { "Identifier", "#FF00FF" },
+			},
+			search = {
+				command = "rg",
+				args = {
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+				},
+				pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+			},
+		})
+	end,
 }
