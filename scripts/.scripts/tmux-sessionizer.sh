290d3dbeb92238a87d9da956bd7aaed5b55eb5d5
HarshK200 <harshkapadia04@gmail.com>
2024-09-28 23:01:39 +0530

    re-wrote the tmux-sessionizer script now can create new sessions from within sessions and attach to prev one's without duplication

--- scripts/.scripts/tmux-sessionizer.sh
+++ scripts/.scripts/tmux-sessionizer.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 1 ]]; then
-    selected=$1
-else
-    selected=$(find ~/Desktop/ ~/Desktop/rust/ /mnt/ /home/ /home/harsh/PC/ /mnt/Data/Personal/ /mnt/Data/configs/ /mnt/Data/Personal/programmingPractice /mnt/Data/Personal/webDev /mnt/Data/Personal/cohort3/ -mindepth 1 -maxdepth 1 -type d | fzf)
+selected=$(find ~/Desktop/ ~/Desktop/rust/ ~/Desktop/go/ /mnt/ /home/ /home/harsh/PC/ /mnt/Data/Personal/ /mnt/Data/configs/ /mnt/Data/Personal/programmingPractice /mnt/Data/Personal/webDev /mnt/Data/Personal/cohort3/ -mindepth 1 -maxdepth 1 -type d | fzf)
+selected_name=$(basename "$selected") # just extracting the name of the session
+
+# checks if user cancled without selecting and kills the command
+if [[ $selected_name == "" ]]; then
+    exit 1
 fi
 
-if [[ -z $selected ]]; then
-    exit 0
+tmux has-session -t="$selected_name" 2>/dev/null
+
+# $? holds the exit status of the last executed command
+if [ $? != 0 ]; then
+    # the -d tag is VERY IMP! this creats a new session in detached mode
+    tmux new-session -d -s "$selected_name"
 fi
 
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
-
-if [[ -z $TMUX ]]; then
-    tmux new-session -s "$selected_name" -c "$selected"
-    exit 0
-fi
-
-if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
-fi
-
+# attaches to selected session if it didn't existed before then it was created in the above if statement
 tmux switch-client -t "$selected_name"
