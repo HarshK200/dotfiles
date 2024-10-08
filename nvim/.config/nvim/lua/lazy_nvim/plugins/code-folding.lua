5ac5dd83a260834500ad459a1c4e8346b54c13fc
HarshK200 <harshkapadia04@gmail.com>
2024-10-08 20:28:13 +0530

    added custom level for code folding so it doesn't go so deep

--- nvim/.config/nvim/lua/lazy_nvim/plugins/code-folding.lua
+++ nvim/.config/nvim/lua/lazy_nvim/plugins/code-folding.lua
@@ -25,11 +25,16 @@ return {
 	},
 	config = function()
 		vim.o.foldcolumn = "1" -- '0' is not bad
-		vim.o.foldlevel = 1 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
+
+        -- just the arrow icons for open close folding
 		vim.cmd([[ set fillchars=foldopen:▾,foldclose:▸ ]])
 		vim.cmd([[ highlight Folded guifg=None guibg=None ]])
+
+        -- Use 'expr' foldmethod with nvim-ufo
+        vim.o.foldmethod = 'expr'
 
 		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 		vim.keymap.set("n", "zr", require("ufo").openAllFolds)
@@ -41,10 +46,14 @@ return {
 			end
 		end)
 
+        -- just setting up ufo with lsp
 		require("ufo").setup({
 			provider_selector = function()
 				return { "lsp" }
 			end,
 		})
+
+        -- setting how DEEP the code folding should be (the higher the number the less DEEPER the code folding if you know what i mean 😏)
+        vim.o.foldminlines = 15
 	end,
 }
