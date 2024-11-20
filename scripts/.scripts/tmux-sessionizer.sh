e00787507c2cf026d77d55a3d2157483db14ce79
HarshK200 <harshkapadia04@gmail.com>
2024-11-20 09:07:16 +0530

    updated rofi theme

--- scripts/.scripts/tmux-sessionizer.sh
+++ scripts/.scripts/tmux-sessionizer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-selected=$(find ~/Desktop/ ~/Desktop/rust/ ~/Desktop/go/ /mnt/ /home/ /home/harsh/PC/ /mnt/Data/Personal/ /mnt/Data/configs/ /mnt/Data/Personal/programmingPractice /mnt/Data/Personal/webDev /mnt/Data/Personal/cohort3/ -mindepth 1 -maxdepth 1 -type d | fzf)
+selected=$(find ~/Desktop/ ~/Desktop/rust/ ~/Desktop/go/ /mnt/ /home/ /mnt/Data/Personal/ /mnt/Data/configs/ /mnt/Data/Personal/programmingPractice /mnt/Data/Personal/webDev /mnt/Data/Personal/cohort3/ -mindepth 1 -maxdepth 1 -type d | fzf)
 selected_name=$(basename "$selected") # just extracting the name of the session
 
 # checks if user cancled without selecting and kills the command
