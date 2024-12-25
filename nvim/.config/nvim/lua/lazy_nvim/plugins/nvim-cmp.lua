1fc2b69336998dc09cb228be6e4e1865b822f68a
HarshK200 <harshkapadia04@gmail.com>
2024-12-25 17:05:47 +0530

    some simple i3wm and and nvim updates

--- nvim/.config/nvim/lua/lazy_nvim/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/lazy_nvim/plugins/nvim-cmp.lua
@@ -37,8 +37,8 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-f>"] = cmp.mapping.scroll_docs(-4),
+				["<C-b>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<C-y>"] = cmp.mapping.confirm({ select = false }),
