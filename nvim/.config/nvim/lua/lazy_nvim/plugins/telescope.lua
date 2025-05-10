17e6558aaadbf01579d6c358f4104766c59beab3
HarshK200 <harshkapadia04@gmail.com>
2025-05-11 03:14:47 +0530

    fixed the telescope native plugin config in telescope.lua

--- nvim/.config/nvim/lua/lazy_nvim/plugins/telescope.lua
+++ nvim/.config/nvim/lua/lazy_nvim/plugins/telescope.lua
@@ -1,49 +1,47 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+		"folke/todo-comments.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    local trouble_telescope = require("trouble.sources.telescope")
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_to_qflist + actions.open_qflist, -- send the telescope search results to quickfix list
+					},
+				},
+				file_ignore_patterns = { "node_modules", "next/" },
+			},
 
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
 
-    telescope.setup({
+			-- To make fzf native work
+			extensions = {
+				fzf = {},
+			},
+		})
 
-      pickers = {
-        find_files = {
-            hidden = true,
-        },
-      },
+		telescope.load_extension("fzf")
 
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist, -- send the telescope search results to quickfix list
-            -- ["<C-t>"] = trouble_telescope.open,
-          },
-        },
-        file_ignore_patterns = { "node_modules",  "next/"}
-      },
-    })
-
-    telescope.load_extension("fzf")
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set("n", "<C-f>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-  end,
+		-- set keymaps
+		vim.keymap.set("n", "<C-f>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor cwd" })
+		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+	end,
 }
