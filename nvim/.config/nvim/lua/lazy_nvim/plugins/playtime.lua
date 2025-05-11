9f7717c41e00f07d87ec967c076bb8a698d22e61
HarshK200 <harshkapadia04@gmail.com>
2025-05-11 15:08:08 +0530

    updated config for playtime.nvim

--- nvim/.config/nvim/lua/lazy_nvim/plugins/playtime.lua
+++ nvim/.config/nvim/lua/lazy_nvim/plugins/playtime.lua
@@ -4,6 +4,23 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		require("playtime").setup()
+		require("playtime").setup({
+			window = {
+				relative = "editor",
+				width = 8,
+				height = 1,
+				row = 0, -- row does defnies where to place on the y axis
+				col = vim.o.columns, -- column does defines where to place on the x axis
+				style = "minimal",
+				focusable = false,
+				noautocmd = true,
+				border = "rounded",
+				anchor = "NW",
+				zindex = 150,
+			},
+			win_visible_on_startup = true, -- the window is visible on startup by default
+		})
+
+		vim.keymap.set("n", "<leader>pt", ":PlaytimeToggle<CR>")
 	end,
 }
