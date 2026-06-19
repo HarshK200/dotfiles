ba724854632ed42d13c2298bee57d4c37a05a621
HarshK200 <harshkapadia04@gmail.com>
2026-06-20 02:06:22 +0530

    removed the annoying netrw t = open new buffer keybind

--- nvim/.config/nvim/lua/kouShinji/autocommands.lua
+++ nvim/.config/nvim/lua/kouShinji/autocommands.lua
@@ -7,6 +7,28 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- for plugins auto update
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = vim.api.nvim_create_augroup("autoupdate", { clear = true }),
+	callback = function()
+		require("lazy").update({ show = false })
+	end,
+})
+
+-- disable that annoying make new buffer in netrw keybind
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "netrw",
+	callback = function(args)
+		for _, map in ipairs(vim.api.nvim_buf_get_keymap(args.buf, "n")) do
+			-- check if keymap is set before deleting
+			if map.lhs == "t" then
+				vim.keymap.del("n", "t", { buffer = args.buf })
+				break
+			end
+		end
+	end,
+})
+
 -- Set indenting to 2 spaces for specific filetypes
 vim.api.nvim_exec2(
 	[[
@@ -18,17 +40,7 @@ vim.api.nvim_exec2(
 	{ output = false }
 )
 
--- NOTE: for plugins auto update
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	group = vim.api.nvim_create_augroup("autoupdate", { clear = true }),
-	callback = function()
-		require("lazy").update({ show = false })
-	end,
-})
-
---NOTE: for cursor change on insert
--- Change cursor to a vertical line in insert mode
+-- change cursor to a vertical line in insert mode
 vim.api.nvim_exec2(
 	[[
   autocmd InsertEnter * silent! execute 'let &t_SI = "\e[6 q"'
